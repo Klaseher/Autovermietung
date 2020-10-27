c85d46eb51cfbcbc55199bdb55e151721343053c
Paul <Klaseher@gmx.de>
2020-10-27 19:55:10 +0100

    klitze kleine Veränderung
    ich hab erstmal die node_modules entfernt, damit das repo übersichtlicher ist. Ich hab das Projekt auch jetzt mit dem cli von vue initialisiert, wodurch wir jetzt javascript statt typescript nutzen.

--- Autovermietung/src/App.vue
+++ Autovermietung/src/App.vue
@@ -1,20 +1,26 @@
 <template>
-    <div id="app">
-        <Home msg="Hello world!" />
-    </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator';
-    import Home from './components/Home.vue';
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
-    @Component({
-        components: {
-            Home
-        }
-    })
-    export default class App extends Vue { }
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
