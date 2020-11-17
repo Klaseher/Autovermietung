df5ed10b9d79a4c64e572034e2e2bb0d2f37005a
Paul <Klaseher@gmx.de>
2020-11-17 16:34:16 +0100

    node_module
    bug mit node modules behoben

--- Autovermietung/src/App.vue
+++ Autovermietung/src/App.vue
@@ -1,26 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <div id="nav">
+    
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">HEYRJP</router-link> |
+    <!-- <router-link to="/user">Login/Register</router-link> | -->
+    <router-link to="/search">Unsere Autos</router-link> | 
+    <router-link to="/login">{{login}}</router-link>
+    <div v-if="loggedIn">
+    <router-link @click="logout" to="/logout">Ausloggen</router-link>
+    </div>
+  </div>
+  <router-view/>
+</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+import Auth from './services/auth.service'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+      loggedIn: false,
+      login: 'Registrieren/Anmelden'
+    }
+  },
+
+methods: {
+  logout(){
+    Auth.logout()
+  },
+   changeValue () {
+      // eslint-disable-next-line eqeqeq
+      if (sessionStorage.getItem('auth') == 'true') {
+        this.login = 'Dein Account'
+        this.loggedIn = true
+      } else {
+        this.login = 'Registrieren/Anmelden'
+        this.loggedIn = false
+      }
+    }
+},
+
+watch: {
+    $route() {
+        this.changeValue()
+    }
+},
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
+  background:black;
+  block-size: 5pc;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 20px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
